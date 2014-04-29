4d2af1b7abd07145fd8cfd94df2bbafa330a16cd
joelterry <jnterry@berkeley.edu>
2014-04-28 23:08:29 -0700

    changed orientation to vertical

---
+++ Girl.h
@@ -0,0 +1,13 @@
+//
+//  Girl.h
+//  Umbrella
+//
+//  Created by Joel Terry on 4/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Girl : CCSprite
+
+@end
