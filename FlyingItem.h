c2e4117d716568b622cd3a3260513eb358405bc1
Logan Carter <logmac@berkeley.edu>
2014-04-28 23:52:18 -0700

    please let this work

---
+++ FlyingItem.h
@@ -0,0 +1,13 @@
+//
+//  FlyingItem.h
+//  Umbrella
+//
+//  Created by Logan Carter on 4/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface FlyingItem : CCSprite
+
+@end
